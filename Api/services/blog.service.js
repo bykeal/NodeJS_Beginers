b0bea0abe73e99872e34d2f75123a3976da2968c
bykeal <okonkwochibuike81@gmail.com>
2022-01-19 19:39:47 +0100

    commit

--- Api/services/blog.service.js
+++ Api/services/blog.service.js
@@ -2,6 +2,8 @@ const BlogModel = require('../model/blog.model');
 class blogService{
    async createPost(data){
     //    console.log('this is the console data',data);
+        const {title, description , author} = data;
+        if (title || description || author ) throw
         const blog = new BlogModel({
             title : data.title,
             description : data.description,
