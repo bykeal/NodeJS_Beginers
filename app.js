282dc45ec7e4f1b6a64bd31315b0235927625119
bykeal <okonkwochibuike81@gmail.com>
2022-01-19 19:20:21 +0100

    commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 
 dotenv.config();
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.wnqd5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.wnqd5.mongodb.net/${process.env.MONGO_DB_PASSWORD}?retryWrites=true&w=majority`);
 
 // mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.wnqd5.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`).then((res)=>{
 //     console.log('connection succesfull');
