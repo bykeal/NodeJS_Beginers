c78316ebd9b0e60f221081944ce52913122c7a2c
bykeal <okonkwochibuike81@gmail.com>
2022-01-21 17:53:20 +0100

    main commit

---
+++ Api/controller/user.controller.js
@@ -0,0 +1,17 @@
+const userService = require('../services/user.service');
+const CustomError = require("../utils/custom.error");
+
+class userController {
+
+    async create(req,res,next){
+         try {
+            const result = await userService.register(req.body);
+            console.log(result);
+            res.status(201).send({success: "true", response : "Welcome" , 
+            data : result}); 
+        } catch (error) {
+            console.log(error);
+            next(error);
+        }
+    }
+}
--- Api/services/user.service.js
+++ Api/services/user.service.js
@@ -1,2 +1,22 @@
-const user = require("../model/user.model");
+const usermodel = require("../model/user.model");
 const CustomError = require("../utils/custom.error");
+
+class userService {
+    async register(data){
+        const {email , name} = data;
+
+        const existingemail = await usermodel.find({email});
+        if(existingemail) throw new CustomError("email already exist!!!");
+
+        const existingname = await usermodel.find({name});
+        if(existingname) throw new CustomError("username already exist!!!");
+
+        const user = new usermodel({
+            name: data.name,
+            email: data.email,
+            password: data.password
+        });
+        return user.save();
+
+    }
+}
