8eb9962b63ab1bf8847b0e423c9744d5e3edd691
bykeal <okonkwochibuike81@gmail.com>
2022-07-23 15:36:22 +0100

    Enhancing

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const Blog = require('./Api/routes/blog.route');
 const User = require('./Api/routes/user.route');
-const app = express();
+var cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const CustomError = require("./Api/utils/custom.error");
+const app = express(); 
 dotenv.config();
+
+var corsOptions = {
+    origin: '*',
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+app.use(cors(corsOptions));
 
 mongoose.connect(`mongodb+srv://chibuike:chibyke1935@cluster0.wnqd5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
     .catch(err => {
